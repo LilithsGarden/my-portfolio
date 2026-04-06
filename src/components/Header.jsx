376e5b04ce46ad8076473835858b5495949dcf94
LilithsGarden <chummie2005@gmail.com>
2026-04-06 17:47:11 +0100

    Initial commit: My portfilio built with react

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header-content">
+                <h1 className="header-title">Chimuanya Mirabel Obioma-Onyenma</h1>
+                <p className="header-subtitle">Software Engineer</p>
+                <p className="header-tagline">I build scalable web applications</p>
+            </div>
+        </header>
+    )   
+}
+
+export default Header
